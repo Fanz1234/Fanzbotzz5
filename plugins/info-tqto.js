bf5d0dca85fffd90f05fa7040b62274c36e64f5c
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2022-11-28 19:37:18 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -67,10 +67,10 @@ https://github.com/ImYanXiao
 
 `
 conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/unknown",
     mediaType: "VIDEO",
-    description: "https://Instagram.com/_ctzhid", 
-    title: 'WH-MODS-DEV-MultiDevice',
+    description: "https://Instagram.com/unknown", 
+    title: 'FanzBotzz',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
