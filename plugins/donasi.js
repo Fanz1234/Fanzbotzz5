ac7aba6cf3ebb60423f455ec4d79ddb469cabfe9
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2022-11-28 23:16:34 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,9 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Indosat:* [${global.ppulsa}]
+❏────
+
 ┌─「 Donasi • Non Pulsa 」
 │ • *Dana:* [${global.pdana}]
 │ • *Gopay:* [${global.pgopay}]
