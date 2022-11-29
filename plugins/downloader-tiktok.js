42b25a024ae450cccce9badd1aa3860d834491bf
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2022-11-29 15:49:47 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,14 +1,12 @@
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    .catch(async _ => await tiktokdlv2(args[0]))
-        .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', 
-`              *「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { tiktok } from "social_media_downloader"
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+if (!args[0]) throw 'Masukkan Link'
+try {
+let p = await tiktok(args[0])
+    if (!p.link) throw 'Can\'t download video!'
+    let cap = `*「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
                  ████████▀▀▀████
                  ████████────▀██
                  ████████──█▄──█
